9f655fb06c7624611b4bbb9aa8dc423d636b2703
plasticine <975036719@qq.com>
2022-09-27 11:32:54 +0800

    feat: nextjs-demo

---
+++ nextjs-demo/components/navbar/index.tsx
@@ -0,0 +1,16 @@
+import logoLight from '@/public/logo_light.png'
+import Image from 'next/image'
+import { FC } from 'react'
+import styles from './styles.module.scss'
+
+export interface INavBarProps {}
+
+export const NavBar: FC<INavBarProps> = () => {
+  return (
+    <div className={styles.navBar}>
+      <a href="http://localhost:3000/">
+        <Image src={logoLight} alt="Demo" width={70} height={20} />
+      </a>
+    </div>
+  )
+}
